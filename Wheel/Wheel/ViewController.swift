301d6617e0917732b5cde5dc846641f95deb17ce
stephaniel96 <stephaniel96@gmail.com>
2016-04-02 02:49:56 -0400

    Xcode base code

---
+++ Wheel/Wheel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wheel
+//
+//  Created by Stephanie Liu on 4/2/16.
+//  Copyright © 2016 Stephanie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
