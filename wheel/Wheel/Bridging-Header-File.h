1d9b645e516e306eecd40ff6e21da8bb264c6459
Maia Ezratty <maiaviolet@gmail.com>
2016-04-03 00:35:13 -0400

    adding obj c files

---
+++ wheel/Wheel/Bridging-Header-File.h
@@ -0,0 +1,17 @@
+//
+//  Bridging-Header-File.h
+//  Wheel
+//
+//  Created by Maia Ezratty on 4/2/16.
+//  Copyright © 2016 Stephanie Liu. All rights reserved.
+//
+
+#ifndef Bridging_Header_File_h
+#define Bridging_Header_File_h
+
+#import "SMRotaryProtocol.h"
+#import "SMRotaryWheel.h"
+#import "SMSector.h"
+#import "SMViewController.h"
+
+#endif /* Bridging_Header_File_h */
